138718b614f89cb5f57f81d00ed6f59efed165a3
bulendsaciragic <bulendsaciragic@outlook.com>
2024-07-27 11:12:26 +0200

    Implement addition functionality for two number inputs

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
-// let num1 = prompt("Enter number 1");
-// let num2 = prompt("Enter number 2");
-// let operator;
+// Klik na dugmad - Update displayvalue
+// klik na operator - store display value to num1
+// Klik na dugman  - reset displayvalue, update display value
+// Klik na jednako - pokrece funkciju num 1 - operator - num2, update display value
 
-// function add(num1, num2) {
-//     return num1 + num2;
-// }
+let num1;
+let num2;
+let displayValue = '';
+let shouldClearDisplay = false;
+let equalsOperation;
 
-// function subtract(num1, num2) {
-//     return num1 num2;
-// }
+function addOperation () {
+    return num1 + num2;
+}
 
-// function multiply(num1, num2) {
-//     return num1 * num2;
-// }
-
-// function divide(num1, num2) {
-//     return num1 / num2;
-// }
-
-// function operate (num1, num2, operator) {
-
-// }
-
-const numbersButtons = document.querySelectorAll(".number")
-const displayValue = document.querySelector("#displayValue");
+const numbersButtons = document.querySelectorAll(".number");
+const operatorsButtons = document.querySelectorAll(".operator");
+const display = document.querySelector("#display");
 const clearButton = document.querySelector("#clear");
 
-const add = document.querySelector("#add");
+const addButton = document.querySelector("#add");
+const equalsButton = document.querySelector("#equals");
 
 numbersButtons.forEach(function(number){
 
     number.addEventListener("click", function(){
+    if (shouldClearDisplay) {
+        display.textContent = ''; // Clear the display
+        displayValue = ''; // Reset displayValue
+        shouldClearDisplay = false; // Reset the flag
+        }
     const numberValue = number.textContent;
+    displayValue += numberValue;
     const newDisplayValue = document.createTextNode(numberValue);
-    displayValue.appendChild(newDisplayValue);
-
+    display.appendChild(newDisplayValue);
     });
 
 });
 
-clearButton.addEventListener("click", function () {
-    displayValue.textContent = " ";
+addButton.addEventListener("click", function(){
+    num1 = Number(displayValue);
+    operator = addOperation;
+    displayValue = '';
+    shouldClearDisplay = true;
 })
+
+equalsButton.addEventListener("click", function(){
+    num2 = Number(displayValue);
+    equalsOperation = num1 + num2;
+    displayValue = equalsOperation;
+    display.textContent = equalsOperation;
+})
+
+clearButton.addEventListener("click", function () {
+    display.textContent = " ";
+    displayValue = ''; 
+});
